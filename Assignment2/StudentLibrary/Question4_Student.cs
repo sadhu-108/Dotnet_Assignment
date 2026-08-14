c548f18b033b5d5e5a0a0e22bb104f19dcc2f195
sadhu-108 <mukund1.bvnm@gmail.com>
2026-08-14 09:20:33 +0530

    Add files via upload

---
+++ Assignment2/StudentLibrary/Question4_Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StudentLibrary
+{
+    public class Student
+    {
+        public string Name;
+        private int Age;
+        protected string Department;
+        internal double CGPA;
+        protected internal string College;
+        private protected string Address;
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Department: " + Department);
+            Console.WriteLine("CGPA: " + CGPA);
+            Console.WriteLine("College: " + College);
+            Console.WriteLine("Address: " + Address);
+        }
+    }
+}
